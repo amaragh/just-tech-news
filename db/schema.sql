3e61971347c32d4895c9812128d6bdd923d1e0b5
amaragh <mercer.alecia@gmail.com>
2022-11-02 22:09:34 -0500

    create user routes

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS just_fetch_news_db;
+CREATE DATABASE just_fetch_news_db;
---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model { }
+
+// define table colums and configuration
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+
+        // define an id column
+        id: {
+            // use the special Sequelize DataTypes object to provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false, we cna run our data through validators before creating the table data
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least 4 characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIG OPTIONS GO HERE
+
+        // PASS IN OUR IMPORTED SEQUELIZE CONNECTION (the direct connection to our database)
+        sequelize,
+        // don't automaticaly create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase in the Database
+        modelName: 'user'
+    }
+);
+
+module.exports = User; 0
