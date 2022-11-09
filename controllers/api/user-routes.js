1c292713415affb4d9b985707b29a6f4af5df080
amaragh <mercer.alecia@gmail.com>
2022-11-08 18:39:29 -0600

    allow user authentication, login, logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,7 +63,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.iser_id = dbUserData.id,
+                    req.session.username = dbUserData.username,
+                    req.session.loggedIn = true;
+
+                res.json(dbUserData)
+            });
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err)
@@ -90,7 +98,13 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            req.session.iser_id = dbUserData.id,
+                req.session.username = dbUserData.username,
+                req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
 
 });
@@ -139,5 +153,17 @@ router.delete('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// log user out
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+})
 
 module.exports = router;
