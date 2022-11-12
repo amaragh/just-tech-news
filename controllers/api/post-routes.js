6413e1e0b8d5c82feb10bf10bdf3457cf5405dc8
amaragh <mercer.alecia@gmail.com>
2022-11-12 14:36:36 -0600

    add front end voting and comment functionality; add views logic

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -94,15 +94,18 @@ router.post('/', (req, res) => {
 });
 
 // PUT /api/posts/upvote ...Defined before the /:id PUT route. Otherwise, Express.js will think "upvote" is a valid parameter for /:id
-router.put('/upvote', (req, res) =>
-    // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err)
-        })
-);
+router.put('/upvote', (req, res) => {
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+});
 
 // update post title
 router.put('/:id', (req, res) => {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,7 +65,7 @@ router.post('/', (req, res) => {
     })
         .then(dbUserData => {
             req.session.save(() => {
-                req.session.iser_id = dbUserData.id,
+                req.session.user_id = dbUserData.id,
                     req.session.username = dbUserData.username,
                     req.session.loggedIn = true;
 
@@ -99,7 +99,7 @@ router.post('/login', (req, res) => {
         }
 
         req.session.save(() => {
-            req.session.iser_id = dbUserData.id,
+            req.session.user_id = dbUserData.id,
                 req.session.username = dbUserData.username,
                 req.session.loggedIn = true;
 
